83b949a61017d83f0fa56cc0f80e0b71ffea52af
Keith Isham <kisham8899@gmail.com>
2019-09-21 14:03:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,3 +7,9 @@
 #     erb :index
 #   end
 # end
+
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+end
